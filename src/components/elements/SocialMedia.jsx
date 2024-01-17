f0afa980aa031fe98f22f0a0930169fa9b54b8c0
mohammadbayatzadeh <mamadbayat777@gmail.com>
2024-01-17 18:24:28 +0330

    link and title done

--- src/components/elements/SocialMedia.jsx
+++ src/components/elements/SocialMedia.jsx
@@ -10,10 +10,10 @@ import X from "../../assets/icons/X";
 function SocialMedia() {
   return (
     <div className={styles.container}>
-      <a href="#">
+      <a href="https://discord.com/invite/Nt7f95bE32" target="_blank">
         <Discord />
       </a>
-      <a href="#">
+      <a href="https://twitter.com/FurriesFrens" target="_blank">
         <X />
       </a>
     </div>
