2f93162d316d3bbf6aabffa21450835a9a0863c9
mohammadbayatzadeh <mamadbayat777@gmail.com>
2024-01-19 18:43:17 +0330

    logo added

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,21 +9,21 @@ import DApp from "../../assets/icons/DApp";
 import Epoch from "../../assets/icons/Epoch";
 import Creator from "../../assets/icons/Creator";
 import NavItem from "../elements/NavItem";
-import Logo from "../../assets/icons/Logo";
+import Logo from "../../assets/icons/logo.svg";
 
 function Header() {
   return (
     <header className={styles.header}>
-      {/* <div className={styles.logo}>
-        <Logo />
-      </div> */}
+      <div className={styles.imageContainer}>
+        <img src={Logo} alt="logo" />
+      </div>
       <nav>
-          <input type="checkbox" id="menu-toggle" className={styles.checkbox} />
-          <label className={styles.hamburger} htmlFor="menu-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
+        <input type="checkbox" id="menu-toggle" className={styles.checkbox} />
+        <label className={styles.hamburger} htmlFor="menu-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
         <ul className={styles.nav}>
           <NavItem title="Staking" icon={<Staking />} link="/staking" />
           <NavItem title="Creator house" icon={<Creator />} link="/creator" />
